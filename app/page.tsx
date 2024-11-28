0d2356670545ce9d188c6f24acfa77dc9d68b76f
poldevy <rostyslavshyian@gmail.com>
2024-11-28 13:58:08 +0100

    finished 49 set up svg

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import { Htag, Button } from "@/components";
 
+import Logo from "./Logo.svg";
+
 export default function Home() {
   return (
     <div>
+      <Logo />
       <Htag tag="h3">sdf</Htag>
       <Button appearance="primary">Hello</Button>
       <Button appearance="ghost">Hello</Button>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config: any) => {
+    // Додайте правило для обробки SVG
+    config.module.rules.push({
+      test: /\.svg$/, // Усі файли з розширенням .svg
+      use: [
+        {
+          loader: '@svgr/webpack', // Для імпорту SVG як React-компоненти
+          options: {
+            svgo: true, // Включити оптимізацію SVG
+            svgoConfig: {
+              plugins: [
+                'preset-default', // Правильне ім'я плагіна
+                {
+                  name: 'removeViewBox', // Зберігаємо viewBox
+                  active: false,
+                },
+              ],
+            },
+          }
+        }
+      ],
+    });
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
