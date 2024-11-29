53878142c8b3fb9f76d4d87009cfd0628424c06c
poldevy <rostyslavshyian@gmail.com>
2024-11-29 12:08:55 +0100

    fixed ptag

--- components/Ptag/Ptag.tsx
+++ components/Ptag/Ptag.tsx
@@ -2,13 +2,11 @@ import { PtagProps } from "./Ptag.Props";
 import styles from "./Ptag.module.css";
 import cn from "classnames";
 
-export const Ptag = ({ size, children }: PtagProps): JSX.Element => {
+export const Ptag = ({ size = "s", children }: PtagProps): JSX.Element => {
   return (
     <p
       className={cn(styles.p, {
-        [styles.s]: size === "s",
-        [styles.m]: size === "m",
-        [styles.l]: size === "l",
+        [styles[size]]: size,
       })}
     >
       {children}
