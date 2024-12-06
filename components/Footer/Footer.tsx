2282b840f2fa35c95bbbf15d06cc6b545535cb8a
poldevy <rostyslavshyian@gmail.com>
2024-12-06 10:56:15 +0100

    finished 77

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,14 @@
 import { FooterProps } from "./Footer.props";
+import cn from "classnames";
+import styles from "./Footer.module.css";
+import { format } from "date-fns";
 
-export const Footer = ({ ...props }: FooterProps) => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps) => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</div>
+      <a href="#">Пользовательское соглашение</a>
+      <a href="#">Политика конфиденциальности</a>
+    </footer>
+  );
 };
